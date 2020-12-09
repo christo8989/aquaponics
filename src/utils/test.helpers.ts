6d5a5fc3354cab7fff698220615212ca1c883d3d
Christopher Jeffery <christo8989@gmail.com>
2020-12-08 21:58:17 -0800

    setup updates

--- src/utils/test.helpers.ts
+++ src/utils/test.helpers.ts
@@ -1,5 +1,5 @@
-import { expect } from "chai";
-import { TestScheduler } from "rxjs/testing";
+import { expect } from 'chai';
+import { TestScheduler } from 'rxjs/testing';
 
 export const testScheduler = new TestScheduler((actual: any, expected: any) => {
   expect(actual).deep.equal(expected);
